73cc91b9f894f6c44dc0fcae8578e771a1a523f1
Lucas Viana <lucasviana112@gmail.com>
2020-02-17 21:35:16 -0300

    configurando para implantação em produção

--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 
-const banco = 'ambiental_urbano';
-const user = 'root';
-const senha = '';
-const server = 'localhost';
+const banco = process.env.DBDATABASE || "ambiental_urbano";
+const user = process.env.DBUSER || "root";
+const senha = process.env.DBPASSWORD;
+const server = process.env.DBHOST || "localhost";
 
-const sequelize = new Sequelize(banco, user, senha,{
-    host: server,
-    dialect: 'mysql',
-    timezone: 'America/Sao_Paulo'
+const sequelize = new Sequelize(banco, user, senha, {
+  host: server,
+  dialect: "mysql",
+  timezone: "America/Sao_Paulo"
 });
 
 module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
-}
+  Sequelize: Sequelize,
+  sequelize: sequelize
+};
